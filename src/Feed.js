da0578fc56df802df3df163a567b583d4c044253
shoaibmasood <shoaib_masood@rocketmail.com>
2020-12-17 23:56:24 +0500

    fixed styleing for feed and added bottom icons

--- src/Feed.js
+++ src/Feed.js
@@ -1,6 +1,11 @@
 import React from "react";
 import "./Feed.css";
 import CreateIcon from "@material-ui/icons/Create";
+import InputOptions from "./InputOptions";
+import ImageIcon from "@material-ui/icons/Image";
+import SubscriptionsIcon from "@material-ui/icons/Subscriptions";
+import EventNoteIcon from "@material-ui/icons/EventNote";
+import CalendarViewDayIcon from "@material-ui/icons/CalendarViewDay";
 
 function Feed() {
   return (
@@ -9,6 +14,20 @@ function Feed() {
         <div className="feed__input">
           <CreateIcon />
           <input placeholder="Start a Post" />
+        </div>
+        <div className="feed__inputOptionss">
+          <InputOptions Icon={ImageIcon} title="Photo" color="#70B5F9" />
+          <InputOptions
+            Icon={SubscriptionsIcon}
+            title="Video"
+            color="#E7A33E"
+          />
+          <InputOptions Icon={EventNoteIcon} title="Event" color="#C0CBCD" />
+          <InputOptions
+            Icon={CalendarViewDayIcon}
+            title="Write article"
+            color="#7FC15E"
+          />
         </div>
       </div>
     </div>
